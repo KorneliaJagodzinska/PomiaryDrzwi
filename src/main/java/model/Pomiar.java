a9e539134c03bf34124acd4125651bf3904a4dcf
Kornelia Jagodzińska <kornelia.jagodzinska@gmail.com>
2020-09-16 13:16:21 +0200

    model

--- src/main/java/model/Pomiar.java
+++ src/main/java/model/Pomiar.java
@@ -1,4 +1,27 @@
 package model;
 
+import jdk.jfr.DataAmount;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Pomiar {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Enumerated(value = EnumType.STRING)
+    private Monter monter;
+    private String klient;
+    @Enumerated(value = EnumType.STRING)
+    private TypDrzwi typDrzwi;
+    private int długość_drzwi;
+    private int szerokość_drzwi;
 }
